747c641b97f8ed1ca8a6b8c7c69627712f8a43d7
peggylai <peggy.elastic@gmail.com>
2017-07-09 04:35:18 +1000

    Initial Commit

---
+++ UserLoginAndRegisteration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserLoginAndRegisteration
+//
+//  Created by peggyLai on 9/7/17.
+//  Copyright © 2017 peggyLai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
